450ca3e0a28d24a93ac0497986c633efb2d7a1a2
hare1039 <hare39393@gmail.com>
2018-04-10 13:13:47 +0800

    Add gdrivedl

---
+++ dl-func.sh
@@ -0,0 +1,108 @@
+trace_file=${HOME}/Documents/git_projects/mega-trace/rate.txt;
+line_buf_file=${HOME}/Documents/git_projects/line-buff/message.txt;
+instance_sub()
+{
+    local rate=$(cat ${trace_file});
+    if [ $rate -eq "0" ]; then
+	echo "Too meny mega instences. Skip.";
+	exit 0;
+    fi
+    # ok, instence - 1
+    let "rate = $rate - 1";
+    echo $rate > ${trace_file};
+}
+instance_plus()
+{
+    local rate=$(cat ${trace_file});
+    # instence + 1
+    let "rate = $rate + 1";
+    echo $rate > ${trace_file};
+}
+
+source ${HOME}/.bash_profile
+export LC_ALL=zh_TW.UTF-8
+instance_sub;
+trap instance_plus EXIT;
+ERROR_CODE=254;
+cd ${HOME};
+
+slack-send()
+{
+    local title=${1##*/};
+    local filename=$2
+    local msg=$(cat <<EOF
+      {
+        "attachments": [
+          {
+            "color": "#36a64f",
+            "pretext": "<https://hare1039.nctu.me/sysvol${1:8}|$title> have updated", 
+            "title": "Downloaded $title:",
+            "title_link": "https://hare1039.nctu.me/sysvol${1:8}",
+            "text": "$filename"
+          }
+        ]
+      }
+EOF
+)
+    slack-send-raw $msg;
+}
+
+gdrivedl_from()
+{
+    local title=${PWD##*/};
+    if [[ "${PWD}" == "${HOME}" ]]; then
+	return $err;
+    fi
+    echo "downloading: $title";
+    local result=$(python3 ~/Documents/git_projects/gdrivedl/dl.py \
+			   -d $PWD                                 \
+			   -s http://192.168.1.120:4445/wd/hub     \
+			   $*)
+    if grep -q -v -E 'ERROR|WARNING' <<< "$result"; then
+	local rawname=$(grep -v -E 'ERROR|WARNING' <<< "$result");
+	local filename;
+	while read -r line; do
+	    filename="$filename \n${line##*/}";
+	done <<< "$rawname";
+	echo $filename;
+	slack-send $PWD $filename;
+	echo "$title have updated";
+	err=0;
+    fi
+    cd ${HOME};
+    return $err;
+}
+
+megadl_from()
+{
+    local title=${PWD##*/};
+    local err=$ERROR_CODE;
+    if [[ "${PWD}" == "${HOME}" ]]; then
+	return $err;
+    fi
+    echo "downloading: $title";
+    local mega_result=$(megadl --no-progress --print-names $* 2>&1);
+    if grep -q 'ENOENT' <<< "$mega_result" ; then
+#	line "$title dl failed. Please renew the url. <<$*>>"
+	echo "$title dl failed. Please renew the url. <<$*>>" > $line_buf_file;
+	echo "$title download failed. Please renew the url. <<$*>>";
+    fi
+    if grep -q -v -E 'ERROR|WARNING' <<< "$mega_result"; then
+	local rawname=$(grep -v -E 'ERROR|WARNING' <<< "$mega_result");
+	local filename;
+	while read -r line; do
+	    filename="$filename \n${line##*/}";
+	done <<< "$rawname";
+#	line "$title have updated.,$filename have been downloaded.";
+	slack-send $PWD $filename
+	echo "$title have updated";
+	err=0;
+    fi
+
+    if grep -q "509" <<< "$mega_result"; then
+        echo "509!";
+    fi
+    find . -type f -size 0 -delete;
+    cd ${HOME};
+    return $err;
+}
--- trace-list.sh
+++ trace-list.sh
@@ -1,131 +1,77 @@
 #!/usr/local/bin/bash
 
-trace_file=${HOME}/Documents/git_projects/mega-trace/rate.txt;
-instance_sub()
-{
-    rate=$(cat ${trace_file});
-    if [ $rate -eq "0" ]; then
-	echo "Too meny mega instences. Skip.";
-	exit 0;
-    fi
-    # ok, instence - 1
-    let "rate = $rate - 1";
-    echo $rate > ${trace_file};
-}
-instance_plus()
-{
-    rate=$(cat ${trace_file});
-    # instence + 1
-    let "rate = $rate + 1";
-    echo $rate > ${trace_file};
-}
-
-source ${HOME}/.bash_profile
-export LC_ALL=zh_TW.UTF-8
-instance_sub;
-trap instance_plus EXIT;
-
-megadl_from()
-{
-    title=${PWD##*/};
-    echo "downloading: $title";
-    mega_result=$(megadl --no-progress --print-names $* 2>&1);
-    if grep -q 'ENOENT' <<< "$mega_result" ; then
-	line "$title dl failed. Please renew the url.";
-	echo "$title download failed. Please renew the url.";
-    fi
-    if grep -q -v 'ERROR' <<< "$mega_result"; then
-	rawname=$(grep -v 'ERROR' <<< "$mega_result");
-#	filename=$(basename ${rawname});
-	while read -r line; do
-	    filename="$filename        ${line##*/}";
-	done <<< "$rawname";
-	line "$title have updated.,$filename have been downloaded.";
-	echo "$title have updated";
-    fi
-#    echo $mega_result > re.txt;
-    if grep -q "509" <<< "$mega_result"; then
-        echo "509!";
-    fi
-    find . -type f -size 0 -delete;
-}
+source ${HOME}/Documents/git_projects/mega-trace/dl-func.sh;
 # unit test #1: ENOENT
 # megadl_from 'https://mega.co.nz/#F!d8RR0RJJ!bdlyZny9iFwJxH2CfXnNTQ';
-cd '/mnt/NAS/video/#連載中/三月的獅子2';
-megadl_from 'https://mega.nz/#F!W8FBjaZI!fIupLoVihVPMrN3TUdBIaQ';
 
 cd '/mnt/NAS/video/#連載中/童話魔法使';
-megadl_from 'https://mega.nz/#F!lXYx3CrD!LU8mavnYWJDqlhsxwNLNcw';
-megadl_from 'https://mega.nz/#F!mRhE1BbJ!gvJWTeABXmMvnN8E1mnZ1g';
-
-cd '/mnt/NAS/video/#連載中/Slow Start';
-megadl_from 'https://mega.nz/#F!ZSBCARjQ!lr4EIJwzNg83Q72qOYqdbA';
-megadl_from 'https://mega.nz/#F!TBoGARqB!VYZr8VKHTrZmZtcWMo5DMA';
-
-cd '/mnt/NAS/video/#連載中/粗點心戰爭2';
-megadl_from 'https://mega.nz/#F!DdAX3KAa!IYLZlgt9yNTGxNPEf2zQTg';
-
-cd '/mnt/NAS/video/#連載中/デスマーチからはじまる異世界狂想曲'
-megadl_from 'https://mega.nz/#F!OQhQlJ5Q!gFuJ8y19pXdRxlzH9fpTIg';
-megadl_from 'https://mega.nz/#F!vExiTaLb!ii0cF8C41EULzNTGgn0EQA';
-
-cd '/mnt/NAS/video/#連載中/からかい上手の高木さん'
-megadl_from 'https://mega.nz/#F!9T5GXQxb!JVIUSgA_Q4Yxgy2_QwHKfA';
-
-cd '/mnt/NAS/video/#連載中/比宇宙更遠的地方'
-megadl_from 'https://mega.nz/#F!PMZAVA7Z!1xxno-iHNV6MRbTiS7Khsg';
+megadl_from 'https://mega.nz/#F!jRJxVLIL!9F_9oCPOSjp7rX2TKVmx-Q';
 
 cd '/mnt/NAS/video/#連載中/庫洛魔法使 透明牌篇';
-megadl_from 'https://mega.nz/#F!f08B1CRA!ML5OO0ODFM4gbOcWJXSeAQ';
+megadl_from 'https://mega.nz/#F!2BpmyZRL!69TDyTv13D-Cuns3IPjmUQ' && cd -;
+megadl_from 'https://mega.nz/#F!f08B1CRA!ML5OO0ODFM4gbOcWJXSeAQ' && cd -;
 megadl_from 'https://mega.nz/#F!25Vh1QKB!-_aNjFO76mgLIeBhNo68SQ';
 
-cd '/mnt/NAS/video/#連載中/ラーメン大好き小泉さん';
-megadl_from 'https://mega.nz/#F!bFIgQQSZ!d9SVLm6ff4Ma7O4Iwt0OHQ';
-
-cd '/mnt/NAS/video/#連載中/citrus';
-megadl_from 'https://mega.nz/#F!qUoilAxA!rYSaMG9XskwYSPvsASNVhQ';
-
-cd '/mnt/NAS/video/#連載中/魔法使的新娘';
-megadl_from 'https://mega.nz/#F!68sA2Q6C!tBElgJtUgzF-7G89Z2L3Wg';
-
-cd '/mnt/NAS/video/#連載中/搖曳露營';
-megadl_from 'https://mega.nz/#F!7A5VXRYZ!n8IgFYSCcRUWsJ8BDW3pew';
-megadl_from 'https://mega.nz/#F!3RAlDKzQ!eO4jmAgNYZcaHjZV0onuNA';
-
-cd '/mnt/NAS/video/#連載中/龍王的工作';
-megadl_from 'https://mega.nz/#F!3Bg02QrL!pQOiI3r7QCoFoxxuvn5Feg';
-
-cd '/mnt/NAS/video/#連載中/博多豚骨ラーメンズ';
-megadl_from 'https://mega.nz/#F!adgXFZDQ!_lnX2p-ZktINPMh1k_kwJg';
-
-cd '/mnt/NAS/video/#連載中/刻刻';
-megadl_from 'https://mega.nz/#F!fcZDWTgT!789gbiFh7MoZAmw_FAZc2A';
+cd '/mnt/NAS/video/#連載中/粗點心戰爭2';
+megadl_from 'https://mega.nz/#F!KZ4l2BiK!hbT4s-Y4e_lCUn-VSag9wg';
 
 cd '/mnt/NAS/video/#連載中/DARLING in the FRANXX'; 
-megadl_from 'https://mega.nz/#F!DYwVGbTY!us3bVpHkt06IE6VrecB8sQ';
-megadl_from 'https://mega.nz/#F!CIpRQDDI!A6kz67aSzRhDJEV4G7-IWg'; 
-megadl_from 'https://mega.nz/#F!ndhW1DiQ!jULC0_GPuNhgftN5r_R_Og';
-megadl_from 'https://mega.nz/#F!PAhViKAb!RRhMlwl-ZPaTR5jXaJTUGA';
+megadl_from 'https://mega.nz/#F!nUZXySwa!3Br1mOuyilFpuDRcVLrapg';
 
 cd '/mnt/NAS/video/#連載中/紫羅蘭永恆花園'; 
-megadl_from 'https://mega.nz/#F!6AZy1YxB!UC9CHe2BHYfGdjMXgtQ99g';
 
 cd '/mnt/NAS/video/#連載中/沒有心跳的少女 BEATLESS';
-megadl_from 'https://mega.nz/#F!OZRGATKS!0QSNkWZBjK49VV-BjL9-eA';
+megadl_from 'https://mega.nz/#F!uEg0jK4S!g4j6qzwISKowREGSUEwZQg';
 
-cd '/mnt/NAS/video/#連載中/戀は雨上がりのように';
-megadl_from 'https://mega.nz/#F!iExDkJCJ!-FDjREAbo3GMQZOAXc_q1w';
+cd '/mnt/NAS/video/#連載中/刀使之巫女';
+megadl_from 'https://mega.nz/#F!kaJFALjS!CanZeJjW7LojwVF3FElpyw';
 
+cd '/mnt/NAS/video/#連載中/老子是魔法少女';
+megadl_from 'https://mega.nz/#F!TYByiCxI!Zz9RMmEFWB0xFMIRSDKTGQ';
+
+cd '/mnt/NAS/video/#連載中/LOST SONG';
+megadl_from 'https://mega.nz/#F!TdoXhJLD!Q88MjXSTybGlYGBajhOs3Q';
+
+cd '/mnt/NAS/video/#連載中/立花館戀愛三角關係'
+megadl_from 'https://mega.nz/#F!jQpDHR7B!KoTx0wOyiRLpR5WlUNCp4w';
+
+cd '/mnt/NAS/video/#連載中/敦君與女朋友';
+#megadl_from 'https://mega.nz/#!HeAwBDgB!YsYzidUG0HfrIFOuluQrzkvPuTU0qV5sejd180DuVTE'
+
+cd '/mnt/NAS/video/#連載中/多田くんは戀をしない';
+megadl_from 'https://mega.nz/#F!6Roj2KJQ!ATYUgg1cqnbdJeZeFjNnUQ';
+
+cd '/mnt/NAS/video/#連載中/黑社會的超能力女兒';
+megadl_from 'https://mega.nz/#F!iHYhCBLC!ZxpklNrUn-gJQcb6c49x9Q';
+
+cd '/mnt/NAS/video/#連載中/魔法少女網站';
+megadl_from 'https://mega.nz/#F!7UAmEIoI!_n8s09A3bsYgasCoIgOcvg';
+
+cd '/mnt/NAS/video/hare/mahou-site';
+gdrivedl_from 'https://drive.google.com/drive/folders/1aH3kpjbohc6VA1XiOhIWrTM1NHIi5i01';
+##<< Not current season >>##
+
+
+
+# cd '/mnt/NAS/NFS/hare1039/download/[CreamPie]√ハーレム！ Root Harem！';
+# megadl_from 'https://mega.co.nz/#!h9RHkDpK!JzBDqWvFoH52OlujucHB6RRM-KZmEr4if1PXYoFwSXY';
+# megadl_from 'https://mega.co.nz/#!904wTQ5Q!Le1-QmBUGngY8tzdJLENFU-qtQl_gxU_VBXKTX5_z7A';
+# cd '/mnt/NAS/video/hare/戀愛ラボ';
+# megadl_from 'https://mega.co.nz/#F!jdZSVRRD!icEhvg_EHxbd5-VY5X_MUA';
+# cd '/mnt/NAS/video/hare';
+# megadl_from 'https://mega.nz/#!qIgnEYqQ!WNFge7qqDUM3eZweP6OJ44gk-B94hP3CJEV3rLZIEKM';
+# cd '/mnt/NAS/video/hare/變態王子與不笑貓';
+# megadl_from 'https://mega.co.nz/#F!RsJQwDJL!GbaAtgnnce8Q444oU26YWA';
+# cd '/mnt/NAS/video/hare/瀨戶的花嫁';
+# megadl_from 'https://mega.co.nz/#F!YBlHGL6Q!6V7jv0fPC4hA9xBKQ-GHFA';
+# # 雪花隨風飄散
 # cd '/mnt/NAS/E_drive/w700desktop/w面白い/#完食/のんのんびより/Season1';
 # megadl_from 'https://mega.co.nz/#F!8wgxlQ7D!YWAhEGgLa3dA5oU3NcSnTQ';
 # 
 # cd '/mnt/NAS/E_drive/w700desktop/w面白い/#完食/のんのんびより/Season2';
 # megadl_from 'https://mega.co.nz/#F!3N1UQJCK!4cQ7ln11t9w-xOl61xFPwg';
-
-
 # cd '/mnt/NAS/NFS/hare1039/download';
 # megadl_from 'https://mega.nz/#!SNJzSLbQ!sVLHGrEr71E2utUI1ZJGvVlDgK6qGVM7M_jffYcEM7o';
-
 # cd '/mnt/NAS/E_drive/これは・・あなただけですよ/bt/animation';
 # # germany1301@eyny獨家
 # megadl_from 'https://mega.nz/#!syozjZTb!JGW8KeW9FvvLKe2xL9I-PbIHhkTSGrTh3SIfAGvNcqg';
@@ -158,6 +104,9 @@ megadl_from 'https://mega.nz/#F!iExDkJCJ!-FDjREAbo3GMQZOAXc_q1w';
 # cd '/mnt/NAS/video/hare/徒然喜歡你';
 # megadl_from 'https://mega.nz/#F!KZsUmB4A!0ckLChMALkVDzRW4DNyo5A';
 # 
+# cd '/mnt/NAS/video/hare/假面男僕';
+# megadl_from 'https://mega.co.nz/#F!koUnDZaC!2L2f5_1ss9e0b7pg9x1i_Q'
+# 
 
 # cd '/mnt/NAS/video/hare/F.AP'; 
 # megadl_from 'https://mega.nz/#F!LNIV3Awa!ltVjBFeDogtyjtcpnKwQFw';
@@ -177,6 +126,9 @@ megadl_from 'https://mega.nz/#F!iExDkJCJ!-FDjREAbo3GMQZOAXc_q1w';
 # megadl_from 'https://mega.co.nz/#F!tpYyjQ5B!VvV9QEh_9FYW7HUkfp7jkg';
 # megadl_from 'https://mega.co.nz/#F!g95nkaaZ!CALWFTJx2Y1jKIkyWqR_8Q';
 # 
+# cd '/mnt/NAS/video/hare/魔法高校的劣等生 呼喚繁星的少女';
+# megadl_from 'https://mega.nz/#!HMBUmC7A!hLsC5WCBOXPyPOwvhL2VQXLjEQkX7tAlHrwqqBPuX2s';
+
 # cd /mnt/NAS/video/#連載中/側車搭檔;
 # megadl_from 'https://mega.nz/#F!wax0ia5J!oLRMhBZ4ay8kMDDY9f9AfQ';
 # 
@@ -245,3 +197,42 @@ megadl_from 'https://mega.nz/#F!iExDkJCJ!-FDjREAbo3GMQZOAXc_q1w';
 # megadl_from "https://mega.co.nz/#!0BpnHZaL!TZR_TFSsyJIVx_hSliVqVQrIinNKdKDtTpJ18ZrdLlw" 
 # megadl_from "https://mega.co.nz/#!RYQWgTbL!cU6Dg0rERdXson2HTasr1VqaV8FRS2vu1xb8OVqk3j8" 
 # 
+# megadl_from 'https://mega.nz/#F!cQsGzKBT!vtMWlN8Em31_CxbwZTpgCQ';
+# cd '/mnt/NAS/video/#連載中/刻刻';
+# megadl_from 'https://mega.nz/#F!fcZDWTgT!789gbiFh7MoZAmw_FAZc2A';
+# cd '/mnt/NAS/video/#連載中/搖曳露營';
+# megadl_from 'https://mega.nz/#F!7A5VXRYZ!n8IgFYSCcRUWsJ8BDW3pew'; cd -;
+# megadl_from 'https://mega.nz/#F!3RAlDKzQ!eO4jmAgNYZcaHjZV0onuNA'; cd -;
+# megadl_from 'https://mega.nz/#F!obIDGIDS!dYVW2K96mllB-lAgZ_Bi7A';
+# cd '/mnt/NAS/video/#連載中/龍王的工作';
+# megadl_from 'https://mega.nz/#F!3Bg02QrL!pQOiI3r7QCoFoxxuvn5Feg';
+# cd '/mnt/NAS/video/#連載中/Slow Start';
+# #megadl_from 'https://mega.nz/#F!lLIDDSpJ!usGEb7ubIqjVVIIyxeP6bQ'; cd -;
+# megadl_from 'https://mega.nz/#F!TBoGARqB!VYZr8VKHTrZmZtcWMo5DMA'; cd -;
+# megadl_from 'https://mega.nz/#F!OMBHER6T!e8ei0Xzi4LesibqsAq9F7g';
+# cd '/mnt/NAS/video/#連載中/比宇宙更遠的地方';
+# megadl_from 'https://mega.nz/#F!PMZAVA7Z!1xxno-iHNV6MRbTiS7Khsg';
+# cd '/mnt/NAS/video/#連載中/ラーメン大好き小泉さん';
+# megadl_from 'https://mega.nz/#F!bFIgQQSZ!d9SVLm6ff4Ma7O4Iwt0OHQ';
+# cd '/mnt/NAS/video/#連載中/citrus';
+# megadl_from 'https://mega.nz/#F!qUoilAxA!rYSaMG9XskwYSPvsASNVhQ';
+# cd '/mnt/NAS/video/#連載中/からかい上手の高木さん';
+# # megadl_from 'https://mega.nz/#F!8Wo2HI6Q!XFH5fWnOncWIl-q3AETMyA'; cd -;
+# megadl_from 'https://mega.nz/#F!iNZwxbAB!ixvGflTukTNuqZe9OI7SeQ';
+# cd '/mnt/NAS/video/#連載中/戀は雨上がりのように';
+# megadl_from 'https://mega.nz/#F!iExDkJCJ!-FDjREAbo3GMQZOAXc_q1w';
+# cd '/mnt/NAS/video/#連載中/デスマーチからはじまる異世界狂想曲'
+# megadl_from 'https://mega.nz/#F!OQhQlJ5Q!gFuJ8y19pXdRxlzH9fpTIg'; cd -;
+# megadl_from 'https://mega.nz/#F!vExiTaLb!ii0cF8C41EULzNTGgn0EQA';
+# cd '/mnt/NAS/video/#連載中/三月的獅子2';
+# megadl_from 'https://mega.nz/#F!W8FBjaZI!fIupLoVihVPMrN3TUdBIaQ';
+# cd '/mnt/NAS/video/hare/膽小狗英雄';
+# megadl_from 'https://mega.nz/#F!oQ1CSRBJ!dZawDVcUoVOsRJUGDLfrSw';
+# cd '/mnt/NAS/video/#連載中/博多豚骨ラーメンズ';
+# megadl_from 'https://mega.nz/#F!adgXFZDQ!_lnX2p-ZktINPMh1k_kwJg';
+# cd '/mnt/NAS/video/#連載中/魔法使的新娘';
+# megadl_from 'https://mega.nz/#F!68sA2Q6C!tBElgJtUgzF-7G89Z2L3Wg';
+# cd '/mnt/NAS/video/hare/The Rolling Girls';
+# megadl_from 'https://mega.co.nz/#F!fcAHkB7Y!Y1OJM7OQU49u77OJJzU11A';
+# cd '/mnt/NAS/video/hare/東京闇鴉';
+# megadl_from 'https://mega.co.nz/#F!N5lkWToY!V-HfOYur7WkPFo5ZdKFj2A';
