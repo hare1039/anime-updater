934d8885728cc8fbef00e1f61b23b53353128049
hare1039 <hare39393@gmail.com>
2018-04-11 11:59:38 +0800

    Correct indent

--- trace.sh
+++ trace.sh
@@ -11,18 +11,18 @@ for row in $(echo "${list}" | jq -r '.[] | @base64'); do
      	echo ${row} | base64 --decode | jq -r ${1}
     }
 
-   #echo $(_jq '.title')
+    #echo $(_jq '.title')
 
-   #echo $(_jq '.file_path');
-   #cd $(_jq '.file_path');
-   for url in $(_jq '.megaurl[]'); do
-	   cd "$(_jq '.file_path')";
-	   #echo "megaurl: $url"
-	   megadl_from $url;
-   done
-   for url in $(_jq '.gdurl[]'); do
-	   cd "$(_jq '.file_path')";
-	   #echo "gdurl: $url"
-	   gdrivedl_from $url;
-   done
+    #echo $(_jq '.file_path');
+    #cd $(_jq '.file_path');
+    for url in $(_jq '.megaurl[]'); do
+	cd "$(_jq '.file_path')";
+	#echo "megaurl: $url"
+	megadl_from $url;
+    done
+    for url in $(_jq '.gdurl[]'); do
+	cd "$(_jq '.file_path')";
+	#echo "gdurl: $url"
+	gdrivedl_from $url;
+    done
 done
