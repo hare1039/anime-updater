c1c124b61b278b947160037b1fbae249c8af6449
User Yinghua <yinghua@2ktarou.lan>
2018-04-12 06:52:07 +0800

    finish slack bot interface with mongodb

--- bot-server/anime-update-bot.go
+++ bot-server/anime-update-bot.go
@@ -12,8 +12,8 @@ import(
 	//"strconv"
 //	"os"
 //	"path/filepath"
-//	"gopkg.in/mgo.v2"
-	//	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"github.com/Jeffail/gabs"
 	"bytes"
 )
@@ -25,8 +25,8 @@ var Host = []string{
 const (
 	Username   = "YOUR_USERNAME"
 	Password   = "YOUR_PASS"
-	Database   = "animeUpdate"
-	Collection = "YOUR_COLLECTION"
+	MGO_Database   = "animeUpdate"
+	MGO_Collection = "YOUR_COLLECTION"
 )
 /////
 type environment struct{
@@ -105,45 +105,26 @@ func redirectHandler(c *gin.Context){
 }
 func sendButtonHandler(c *gin.Context){
 	c.String(http.StatusOK, "")
+
+//	x, err := ioutil.ReadAll(c.Request.Body)
+//	if(err != nil){
+//		panic(err)
+//	}
+//        log.Println("Postbody:" + string(x))
+	
+	
 	token := c.PostForm("token")
 	response_url := c.PostForm("response_url")
+	team_id := c.PostForm("team_id")
+	user_id := c.PostForm("user_id")
+	
 	if(token != env.TOKEN){
 		c.String(http.StatusForbidden, "Access forbidden")
 	} else {
-		message := []byte(`{
-            "text": "This is your first interactive message",
-            "attachments": [
-                {
-                    "text": "Building buttons is easy right?",
-                    "fallback": "Shame... buttons aren't supported in this land",
-                    "callback_id": "button_tutorial",
-                    "color": "#3AA3E3",
-                    "attachment_type": "default",
-                    "actions": [
-                        {
-                            "name": "yes",
-                            "text": "yes",
-                            "type": "button",
-                            "value": "yes"
-                        },
-                        {
-                            "name": "no",
-                            "text": "no",
-                            "type": "button",
-                            "value": "no"
-                        },
-                        {
-                            "name": "maybe",
-                            "text": "maybe",
-                            "type": "button",
-                            "value": "maybe",
-                            "style": "danger"
-                        }
-                    ]
-                }
-            ]
-        }`)
-		sendMessageToSlackResponseURL(response_url, message)
+		messageJSON := checkSuscribeJSON(team_id, user_id)
+		sendMessageToSlackResponseURL(response_url, []byte(messageJSON.String()))		
+
+
 	}
 	
 }
@@ -151,16 +132,56 @@ func actionHandler(c *gin.Context){
 	c.String(http.StatusOK, "")
 	payload := c.PostForm("payload")
 	payloadJSON, _ := gabs.ParseJSON([]byte(payload))
-//	log.Println(payloadJSON.StringIndent("", "    "))
+	log.Println(payloadJSON.StringIndent("", "    "))
 	response_url, _ := payloadJSON.Path("response_url").Data().(string)
-	user_name, _ := payloadJSON.Path("user.name").Data().(string)
-	action_array,_ :=payloadJSON.Path("actions").Children()
-	action_array_name, _ := action_array[0].Path("name").Data().(string)
+	//user_name, _ := payloadJSON.Path("user.name").Data().(string)
 
-	messageJSON := gabs.New()
-	messageJSON.SetP(user_name + " clicked: " + action_array_name, "text")
-	messageJSON.SetP(false, "replace_original")
-	log.Println(messageJSON.String())
+	team_id, _ := payloadJSON.Path("team.id").Data().(string)
+	user_id, _ := payloadJSON.Path("user.id").Data().(string)
+	
+	action_array,_ :=payloadJSON.Path("actions").Children()
+	button_name, _ := action_array[0].Path("name").Data().(string)
+	animeTitle, _ := payloadJSON.Path("callback_id").Data().(string)
+	
+
+	////DB
+	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs: Host,
+		// Username: Username,
+		// Password: Password,
+		// Database: Database,
+		// DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
+		// 	return tls.Dial("tcp", addr.String(), &tls.Config{})
+		// },
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	// Collection
+	collection := session.DB(MGO_Database).C(team_id + ":" + user_id)
+	if(button_name == "Subscribe" ){
+		err = collection.Insert(bson.M{"title": animeTitle})
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		err = collection.Remove(bson.M{"title": animeTitle})
+		if err != nil {
+			panic(err)
+		}
+	}
+	
+	/////DB
+
+	
+//	messageJSON := gabs.New()
+//	messageJSON.SetP(user_name + " clicked: " + action_array_name, "text")
+//	messageJSON.SetP(false, "replace_original")
+	//	log.Println(messageJSON.String())
+
+	messageJSON := checkSuscribeJSON(team_id, user_id)
+
 	sendMessageToSlackResponseURL(response_url, []byte(messageJSON.String()))
 }
 func sendMessageToSlackResponseURL(url string, message []byte){
@@ -174,4 +195,81 @@ func sendMessageToSlackResponseURL(url string, message []byte){
 		panic(err)
 	}
 	defer resp.Body.Close()
+}
+func checkSuscribeJSON(team_id string, user_id string)( *gabs.Container ){
+	///read list json
+	dat, err := ioutil.ReadFile("../list.json")
+	if err != nil{
+		panic(err)
+	}
+	listJSON, err := gabs.ParseJSON(dat)
+	listArray, _ := listJSON.Children()
+
+	///CHECK DB
+	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs: Host,
+		// Username: Username,
+		// Password: Password,
+		// Database: Database,
+		// DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
+		// 	return tls.Dial("tcp", addr.String(), &tls.Config{})
+		// },
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	// Collection
+	collection := session.DB(MGO_Database).C(team_id + ":" + user_id)
+
+	
+
+
+
+	
+	///MAKE JSON
+//	log.Printf("id: " + team_id + " " + user_id)
+	messageJSON := gabs.New()
+	messageJSON.SetP("以下是承皓肥宅最近在追的番，訂閱以獲得更新通知。", "text")
+	messageJSON.SetP(true, "replace_original")
+	messageJSON.Array("attachments")
+	
+
+	for _, child := range listArray {
+		animeTitle := child.Path("title").Data().(string)
+
+		animeJSON :=gabs.New()
+		animeJSON.SetP(animeTitle, "title")
+		animeJSON.SetP(child.Path("title").Data().(string), "callback_id")
+		animeJSON.SetP("default", "attachment_type")
+		animeJSON.SetP("#42f4b9","color")
+		animeJSON.Array("actions")
+		
+		suscribeButton := gabs.New()
+		count, err := collection.Find(bson.M{"title": animeTitle}).Count()
+		if err != nil{
+			panic(err)
+		}
+		if(count > 0){
+			animeJSON.SetP("已訂閱", "text")
+			suscribeButton.SetP("Unsubscribe", "name")
+			suscribeButton.SetP("Unsubscribe", "text")
+			suscribeButton.SetP("button", "type")
+			suscribeButton.SetP("Unsubscribe", "value")
+			suscribeButton.SetP("danger", "style")
+		} else {
+			suscribeButton.SetP("Subscribe", "name")
+			suscribeButton.SetP("Subscribe", "text")
+			suscribeButton.SetP("button", "type")
+			suscribeButton.SetP("Subscribe", "value")
+			suscribeButton.SetP("primary", "style")
+		}
+
+		animeJSON.ArrayAppendP(suscribeButton.Data(), "actions")
+		
+		messageJSON.ArrayAppendP(animeJSON.Data(), "attachments")
+	}
+	//log.Println(messageJSON.StringIndent("", "    "))
+	return messageJSON
+
 }
